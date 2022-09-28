eaf490a8689a23d591aaa170675fae78cb3fa4ff
Philippe de Lanteuil <106361817+PhilippedeLanteuil@users.noreply.github.com>
2022-09-28 16:43:41 -0300

    Add files via upload

---
+++ Comparacao.pas
@@ -0,0 +1,13 @@
+Program comparacao;
+ var x, y: integer;
+ Begin
+ Writeln('Escreva dois números :');
+ Readln(x, y);
+ if (x>y) then
+ Writeln ('X é maior que Y, e seu valor é = ', x)
+ else
+ Writeln ('Y é maior que X, e seu valor é = ', y);
+ readkey;
+ 
+  
+ End.
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_sub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subtracao sub = new Subtracao();
+            sub.setN1(70);
+            sub.setN2(40);
+            sub.setSub(sub.getN1() - sub.getN2());
+            Console.WriteLine(sub.getSub());
+            Console.ReadKey();
+
+            Subtracao subtrai = new Subtracao();
+            subtrai.setN1(50);
+            subtrai.setN2(30);
+            subtrai.setSub(subtrai.getN1() - subtrai.getN2());
+            Console.WriteLine(subtrai.getSub());
+            Console.ReadKey();
+        }
+    }
+}
