5e3aaa69f7a6acee6d34656428cdb274d35ea92f
Philippe de Lanteuil <106361817+PhilippedeLanteuil@users.noreply.github.com>
2022-09-28 16:54:22 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,24 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Classe_sub
+namespace RecebeNome
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Subtracao sub = new Subtracao();
-            sub.setN1(70);
-            sub.setN2(40);
-            sub.setSub(sub.getN1() - sub.getN2());
-            Console.WriteLine(sub.getSub());
-            Console.ReadKey();
-
-            Subtracao subtrai = new Subtracao();
-            subtrai.setN1(50);
-            subtrai.setN2(30);
-            subtrai.setSub(subtrai.getN1() - subtrai.getN2());
-            Console.WriteLine(subtrai.getSub());
+            string nome;
+            Console.WriteLine("Digite um nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine(nome);
             Console.ReadKey();
         }
     }
